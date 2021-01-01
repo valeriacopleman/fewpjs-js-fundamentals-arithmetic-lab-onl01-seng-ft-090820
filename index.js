ca6da0e88b0478d2bcad5c714c145c8ebcebf093
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-01 12:11:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1*num2
+
+
+
+let random = Math.floor(Math.random() ) + 1;
+
+let mod = 12%8
+
+let max = Math.max(1, 2, 3, 4, 20);
